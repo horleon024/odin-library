0d21f7b5ce70c596db0487cec6cac3c35fda7721
horleon024 <horeftosleon@gmail.com>
2022-05-26 11:46:18 +0200

    create popup form

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ function Book(title, author, nbOfPages, isRead) {
 		return (`${this.title} by ${this.author}, ${nbOfPages} pages, ${isRead ? "already read" : "not read yet"}`);
 	}
 }
+
+const form = document.querySelector(".form-popup");
+const newBookButton = document.querySelector(".newbook");
+
+function toggleDisplay() {
+	form.style.display = "block";
+}
+
+newBookButton.addEventListener("click", toggleDisplay);
